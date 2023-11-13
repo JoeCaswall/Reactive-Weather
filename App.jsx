8bc4c0d8c874bb967548e35d6c26f8e902f3c0b0
JoeCaswall <joe.caswall@gmail.com>
2023-11-13 15:54:26 +0000

    monday tasks done

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// Import data and WeatherCard here
+import cities from "./data";
+import WeatherCard from "./components/WeatherCard";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((city) => (
+          <WeatherCard key={city.name} city={city} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
